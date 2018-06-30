5295f9b7c98bd9a51b9d3596225f95acc914e7d7
Josias Kabore <jkabore1@gmail.com>
2018-06-30 11:18:17 -0400

    worked on log in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 import "./Login.css"
 
+class Login extends {
+    state = {
+        data
+  username="",
+ password="",
 
+  }
+  
+   //handle any changes to the input field
+   handleInputChange=event=>{
+    // Pull the name and value properties off of the event.target (the element which triggered the event)
+    const{name, value}=event.target;
+    // Set the state for the appropriate input field
+    this.setState({
+        [name]:value
+    });
+};
+handleFormSubmit=event=>{
+    event.preventDefault();
+    if(this.state.username && this.state.password){
+userAPI.getUser({
+  username:this.state.username,
+  
+  password:this.state.password
 
-        const Login=props=>(
+})
+.catch(err => console.log(err))
+        render(){
+        return(
             <div className="form-inline">
             <h2>Log in </h2>
             
@@ -32,7 +58,7 @@ import "./Login.css"
                 </button>
                 
                 </div>
-        )
-    
+        )}
+    }
 
 export default Login;
--- src/components/newUser/newUser.js
+++ src/components/newUser/newUser.js
@@ -1,6 +1,53 @@
 import React from "react";
 import "./newUser.css";
-const newUser=props=>(
+
+class newUser extends Component {
+  state = {
+    firstName="",
+    lastName="",
+    username="",
+        password="",
+
+  }
+  // Loads userdata  
+  loadUserData = () => {
+    API.getUser()
+      .then(res =>
+        this.setState({ firstName: res.dat, lastName: "", username: "", password: "" })
+      )
+      .catch(err => console.log(err));
+
+  // When the component mounts, 
+  componentDidMount() {
+    this.loadUserData();
+  }
+}
+   //handle any changes to the input field
+   handleInputChange=event=>{
+    // Pull the name and value properties off of the event.target (the element which triggered the event)
+    const{name, value}=event.target;
+    // Set the state for the appropriate input field
+    this.setState({
+        [name]:value
+    });
+};
+handleFormSubmit=event=>{
+    event.preventDefault();
+    
+userAPI.saveUser({
+  firstName:this.state.firstName,
+  lastName:this.state.lastName,
+  username:this.state.username,
+  password:this.state.password
+
+}).then(res => this.loadUserData()
+.catch(err => console.log(err))
+
+     
+    
+}
+render(){
+    return(
     <div className="form-inline">
     <h2>sign up </h2>
     <div className="form-group">
@@ -49,6 +96,6 @@ const newUser=props=>(
         </button>
         </div>
     
-);
+)};
 export default newUser;
 
