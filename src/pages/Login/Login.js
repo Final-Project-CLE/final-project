9e603a0a32099a3d0f9ba1e88c9a90e213fa147e
Josias Kabore <jkabore1@gmail.com>
2018-07-02 16:19:44 -0400

    updating states on forms

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -10,15 +10,60 @@ class Login extends Component {
     username: "",
     password: ""
   };
+ //handle any changes to the input field
+ handleInputChange=event=>{
+  // Pull the name and value properties off of the event.target (the element which triggered the event)
+  const{name, value}=event.target;
+  // Set the state for the appropriate input field
+  this.setState({
+      [name]:value
+  });
+};
+handleFormSubmit=event=>{
+  event.preventDefault();
+  if(this.state.username && this.state.password){
+userAPI.getUser({
+username:this.state.username,
 
+password:this.state.password
+
+})
+.catch(err => console.log(err))}}
 
 
   render() {
     return (
-      <div>
-        <p>Login Page</p>
-      </div>
-    );
+      <div className="form-group">
+      <h3>Log in </h3>
+      
+          <div className="form-group">
+          <input className="form-control"
+          value={this.state.username}
+          name="username"
+          onChange={this.handleInputChange}
+          type="text"
+
+          placeholder="Username"
+          />
+          </div>
+          <div className="form-group">
+          <input className="form-control"
+          value={this.state.password}
+          onChange={this.handleInputChange}
+          type="text"
+          name="password"
+
+          placeholder="password"/>
+          </div>
+          <button className="btn btn-primary"
+          onClick={this.handleFormSubmit}
+          type="button">
+          Submit
+          </button>
+          
+          </div>
+  )
+    
   }
 }
 
--- src/pages/NewUser/newUser.js
+++ src/pages/NewUser/newUser.js
@@ -10,17 +10,72 @@ class newUser extends Component {
     username: "",
     password: ""
   };
+//handle any changes to the input field
+handleInputChange=event=>{
+  // Pull the name and value properties off of the event.target (the element which triggered the event)
+  const{name, value}=event.target;
+  // Set the state for the appropriate input field
+  this.setState({
+      [name]:value
+  });
+};
+handleFormSubmit=event=>{
+  event.preventDefault();
+  
+userAPI.saveUser({
+username:this.state.username,
+email:this.state.email,
 
+password:this.state.password
+
+})
+.catch(err => console.log(err))}
 
   render() {
     return (
 
-            <div>
-              <p>New User Page</p>
-            </div>
+      <div className="form-group">
+          <div className="form-group">
+          <input className="form-control"
+          value={this.state.username}
+          name="username"
+          onChange={this.handleInputChange}
+          type="text"
+  
+          placeholder="username"
+          />
+          </div>
+      
+          <div className="form-group">
+          <input className="form-control"
+          value={this.state.email}
+          name="email"
+          onChange={this.handleInputChange}
+          type="text"
+  
+          placeholder="email"
+          />
+          </div>
+          <div className="form-group">
+          <input className="form-control"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleInputChange}
+          type="text"
+  
+          placeholder="password"/>
+          </div>
+          <div className="form-group">
+          <button className="btn btn-primary"
+          onClick={this.handleFormSubmit}
+          type="button">
+          submit
+          </button>
+          </div>
+          </div>
 
 
-    );
+    )
   }
 }
 
